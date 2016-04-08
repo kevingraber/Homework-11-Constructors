5955e1d9a3dd416569c43fc6a28283047271b51a
kg <kjg310@gmail.com>
2016-04-08 13:14:23 -0400

    prompt working with constructor

---
+++ student.js
@@ -0,0 +1,36 @@
+var prompt = require('prompt')
+prompt.start();
+
+function student(name, gender, grade, gpa, detentions, sleepingInClass, catchPhrase) {
+	this.name = name;
+	this.gender = gender;
+	this.grade = grade;
+	this.gpa = gpa;
+	this.detentions = detentions;
+	this.sleepingInClass = sleepingInClass;
+	this.catchPhrase = catchPhrase;
+	this.canStudentHaveFun = function() {
+		if (this.detentions < 10 && this.gpa > 2) {
+			console.log('You can have fun!')
+		} else {
+			console.log("NO FUN FOR YOU")
+		}
+	}
+}
+
+var kevin = new student('kevin', 'guy', '10', 4, 0, 0, 'hey dudes!');
+console.log("Can Kevin have fun?")
+kevin.canStudentHaveFun()
+
+var darby = new student('darby', 'girl', '10', 1, 12, 0, 'hey dudettes!')
+console.log("Can darby have fun?")
+darby.canStudentHaveFun()
+
+prompt.get(['name', 'gender', 'grade', 'gpa', 'detentions', 'sleepingInClass', 'catchPhrase'], function(err, result) {
+
+	var newKid = new student(result.name, result.gender, result.grade, result.gpa, result.detentions, result.sleepingInClass, result.catchPhrase)
+
+	console.log(newKid)
+	newKid.canStudentHaveFun();
+
+})
